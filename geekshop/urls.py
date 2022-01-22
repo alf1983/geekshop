0835360a2bce85e69aaa88bcde3226d82cfe571c
Vasily <berkach@gmail.com>
2022-01-22 12:51:13 +0200

    lesson2

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from .views import index, contacts
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('contact/', contacts),
+    path('', index, name="index"),
+    path('contacts/', contacts, name="contacts"),
     path('products/', include('mainapp.urls')),
 ]
--- geekshop/views.py
+++ geekshop/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    title = "магазин"
+    context = {
+        'title': title
+    }
+    return render(request, 'index.html', context=context)
 
 
 def contacts(request):
-    return render(request, 'contact.html')
+    title = "наши контакты"
+    context = {
+        'title': title
+    }
+    return render(request, 'contact.html', context=context)
